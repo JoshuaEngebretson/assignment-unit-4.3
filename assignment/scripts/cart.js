83cf41bf1e7ab94cf869a65d0bf6975158472f3c
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-03-15 12:11:41 -0500

    Additional work on addItem and removeItem, still not working properly

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,38 +4,42 @@ console.log('***** Cart Functions *****');
 
 // Create a global variable named basket and set it to an empty array.
 let basket = []
+const maxItems = 5
 console.log('Your basket contains:', basket);
+console.log('The max items in your basket is', maxItems);
 
 // Create a function called addItem. It should:
     // take an input parameter for a string item
     // add the new item to the global array basket.
     // return true indicating the item was added
 function addItem(item){
-    console.log('--- In addItem function ---');
-    // if (!isFull === true) {
-    //     return false+': Your basket is full, '+ item+' was NOT added'
-    // }
+    console.log('--- addItem ---');
+    console.log(isFull(basket));
+    if (isFull === true) {
+        return `${false}: Your basket is full, [${item}] was NOT added`
+    }
     basket.push(item)
-    return true+': '+ item+ ' was added to the basket, your basket now contains: ['+basket+']'
+    return `${true}: [${item}] was added to the basket`
 }
 
 console.log(addItem('socks'));
 console.log(addItem('t-shirt'));
 console.log(addItem(1));
-basket_removed=basket.pop();
+basket_removed = basket.pop();
 console.log('Oops, that not an item. Removed', basket_removed, 'from basket.');
+console.log(addItem('shorts'));
 console.log('Your basket now contains: ['+basket+']');
 
 // Create a function called listItems. It should:
     // loop over the items in the basket array
     // console.log each individual item on a new line
 function listItems(basket){
-    console.log('--- In listItems ---');
+    console.log('--- listItems ---');
     for (let i = 0; i < basket.length; i++) {
         let items_in_basket = basket[i]
         console.log(items_in_basket);
     }
-    return 'The above items are in your basket'
+    return `The above items are in your basket`
 }
 
 console.log(listItems(basket));
@@ -43,12 +47,13 @@ console.log(listItems(basket));
 // Create a function called empty. It should:
     // reset the basket to an empty array
 function empty(basket){
-    console.log('--- In empty function ---');
+    console.log('--- empty ---');
     basket.length = 0
     return basket
 }
 
 console.log('You have emptied your basket of all contents. Your basket is now:', empty(basket));
+console.log(basket);
 
 
 // Stretch Goals
@@ -57,19 +62,21 @@ console.log('You have emptied your basket of all contents. Your basket is now:',
 // Using functions in other functions!
 
 // Add a global const named maxItems and set it to 5.
+/*
 const maxItems = 5
+moved to start of codeblock to try stretch goal of changing addItem to reference isFull
+*/
 
 // Create a function called isFull(). It should:
     // return false if the basket contains less than max number of items
     // return true otherwise (equal or more than maxItems)
 
 function isFull(basket){
-    console.log('--- In isFull function ---');
-    console.log('The max items in your basket is', maxItems);
+    console.log('--- isFull ---');
     if (basket.length < maxItems){
-        return false
+        return `${false}, your basket is NOT full`
     }
-    return true
+    return `${true}, your basket IS full`
 }
 
 console.log(addItem('jeans'));
@@ -98,22 +105,18 @@ console.log('isFull - Expected true:',isFull(basket), basket);
     // Use Array.splice to remove the first matching item from the basket.
     // Return the item removed or null if the item was not found
 function removeItem(item_to_remove){
+    console.log('--- removeItem ---');
     for (let i = 0; i < basket.length; i++) {
-        if (basket.indexOf(item_to_remove) === basket[i]){
-            basket.splice(i,1)
-            return `${item_to_remove} has been removed from the basket`
+        if (basket[i] === item_to_remove){
+            basket.splice[i,1]
+            return `${item_to_remove} has been removed from the basket. Your basket now contains: ${basket}`
+        }
+        else {
+            i++
         }
     }
-    return null
+    return `${null} --- ${item_to_remove} was not found. Your basket contains: ${basket}`
 }
 
-function find( Searching_array, value_to_find ){
-    for (let i=0; i<Searching_array.length; i++) {
-      if (value_to_find === Searching_array[i]) {
-        console.log(value_to_find, 'is a part of', Searching_array, ' at array.length='+ i);
-        return true 
-      } 
-    }
-    console.log(value_to_find, 'is not a part of', Searching_array);
-    return false
-  }
+console.log(removeItem('socks'));
+console.log(basket);
